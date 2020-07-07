a342691c25148bfe5c1073f9dd5d68e273d7034f
Stratos-Chen <twinschen55@gmail.com>
2020-07-07 08:48:23 -0700

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,75 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message}}</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome",
+      places: [],
+      errors: [],
+      newPlaceName: "",
+      newPlaceAddress: "",
+      currentPlace: {}
+    };
+  },
+  created: function() {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function() {
+      axios.get("/api/places").then(response => {
+        console.log("All Places:", response.data);
+        this.places = response.data;
+      });
+    },
+    createPlace: function() {
+      var params = {
+        name: this.newPlaceName,
+        address: this.newPlaceAddress
+      };
+      axios
+        .post("/api/places", params)
+        .then(response => {
+          console.log("Success", response.data);
+          this.places.push(response.data);
+        })
+        .catch(error => {
+          console.log(error.response.data.errors);
+          this.errors = error.response.data.errors;
+        });
+    },
+    showPlace: function(place) {
+      console.log(place);
+      this.currentPlace = place;
+      document.querySelector("#place-details").showModal();
+    },
+    updatePlace: function(place) {
+      var params = {
+        name: place.name,
+        address: place.address
+      };
+      axios
+        .patch(`/api/palces/${place.id}`, params)
+        .then(response => {
+          console.log("Successful update", response.data);
+        })
+        .catch(error => {
+          console.log(error.response.data.errors);
+        });
+    },
+    destroyPlace: function(place) {
+      axios.delete(`/api/palces/${place.id}`).then(response => {
+        console.log("Successful delete", response.data);
+        var index = this.places.indexOf(place);
+        this.places.splice(index, 1);
+      });
+    }
   }
-}
+};
 </script>
